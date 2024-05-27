da0eec66635b664d3872f882228581ff09afab9f
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-05-27 17:37:28 +0530

    Completed footer with responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
 import "./style.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+        <div className="infoText">
+          Explore the world of cinema with our movies demo web app! Discover new
+          releases, browse through classic favorites, and create your
+          personalized watchlist. From action-packed thrillers to heartwarming
+          dramas, our web app brings the magic of movies right to your
+          fingertips. Start your cinematic journey today!.
+        </div>
+        <div className="socialIcons">
+          <span className="icon">
+            <FaFacebookF />
+          </span>
+          <span className="icon">
+            <FaInstagram />
+          </span>
+          <span className="icon">
+            <FaTwitter />
+          </span>
+          <span className="icon">
+            <FaLinkedin />
+          </span>
+        </div>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;
