e5e5fda1c8b9177049ab78dbe42007f6f1bc4c7f
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-05-29 15:26:30 +0530

    In HomePage completed Popular & Top Rated section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,13 +3,16 @@ import "./style.scss";
 
 import HeroBanner from "./heroBanner/HeroBanner";
 import Trending from "./trending/Trending";
+import Popular from "./popular/Popular";
+import TopRated from "./topRated/TopRated";
 
 const Home = () => {
   return (
     <div className="homePge">
       <HeroBanner />
       <Trending />
-      <div style={{ height: 1000 }}></div>
+      <Popular />
+      <TopRated />
     </div>
   );
 };
