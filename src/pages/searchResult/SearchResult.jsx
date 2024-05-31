1f53e0416daa2be9fe1e8985e544568eaea6fda3
nirmalakoliya <nirmal.akoliya@engineering.gigakelvin.com>
2024-05-31 16:02:35 +0530

    some changes in SearchResult page and scss file

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -8,7 +8,6 @@ import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
 import MovieCard from "../../components/movieCard/MovieCard";
 import Spinner from "../../components/spinner/Spinner";
-import noResults from "../../assets/no-results.png";
 
 const SearchResult = () => {
   const [data, setData] = useState(null);
@@ -49,40 +48,38 @@ const SearchResult = () => {
   }, [query]);
 
   return (
-    <>
-      <div className="searchResultsPage">
-        {loading && <Spinner initial={true} />}
-        {!loading && (
-          <ContentWrapper>
-            {data?.results?.length > 0 ? (
-              <>
-                <div className="pageTitle">
-                  {`Search ${
-                    data?.total_results > 1 ? "results" : "result"
-                  } of '${query}'`}
-                </div>
-                <InfiniteScroll
-                  className="content"
-                  dataLength={data?.results?.length || []}
-                  next={fetchNextPageData}
-                  hasMore={pageNum <= data?.total_pages}
-                  loader={<Spinner />}
-                >
-                  {data?.results.map((item, index) => {
-                    if (item.media_type === "person") return;
-                    return (
-                      <MovieCard key={index} data={item} fromSearch={true} />
-                    );
-                  })}
-                </InfiniteScroll>
-              </>
-            ) : (
-              <span className="resultNotFound">Sorry, Results not found!</span>
-            )}
-          </ContentWrapper>
-        )}
-      </div>
-    </>
+    <div className="searchResultsPage">
+      {loading && <Spinner initial={true} />}
+      {!loading && (
+        <ContentWrapper>
+          {data?.results?.length > 0 ? (
+            <>
+              <div className="pageTitle">
+                {`Search ${
+                  data?.total_results > 1 ? "results" : "result"
+                } of '${query}'`}
+              </div>
+              <InfiniteScroll
+                className="content"
+                dataLength={data?.results?.length || []}
+                next={fetchNextPageData}
+                hasMore={pageNum <= data?.total_pages}
+                loader={<Spinner />}
+              >
+                {data?.results.map((item, index) => {
+                  if (item.media_type === "person") return;
+                  return (
+                    <MovieCard key={index} data={item} fromSearch={true} />
+                  );
+                })}
+              </InfiniteScroll>
+            </>
+          ) : (
+            <span className="resultNotFound">Sorry, Results not found!</span>
+          )}
+        </ContentWrapper>
+      )}
+    </div>
   );
 };
 
